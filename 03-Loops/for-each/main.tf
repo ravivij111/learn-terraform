5ffb22e794d27ec5bd021c69b1e9dccd80e97f7e
Your Name <you@example.com>
2023-05-12 06:11:42 +0800

    Terraform

--- 03-Loops/for-each/main.tf
+++ 03-Loops/for-each/main.tf
@@ -1,29 +1,27 @@
-#resource "null_resource" "null" {
-#  count =10
-#
-#}
-# *************** Start of Count *************************
-#variable "fruits" {
-#  default = {
-#    "apple", "orange", "banana"
-#  }
-#}
-#
-#resource "null_resource" "fruits" {
-#  count = length(var.fruits)
-#  provisioner "local-exec" {
-#    command = "echo Fruit Name - ${var.fruits[count.index]}"
-#  }
-#}
-# *************** End of Count *************************
+/*resource "null_resource" "null" {
+  count =10
+
+}
+ *************** Start of Count *************************
+variable "fruits" {
+  default = {
+    "apple", "orange", "banana"
+  }
+}
+
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${var.fruits[count.index]}"
+  }
+}
+ *************** End of Count **************************/
 
 
 # *************** Start of for_each *************************
-variable "fruits1" {
+/*variable "fruits1" {
   default = {
-    apple = 10
-    orange = 15
-    banana =20
+    "apple", "orange", "banana"
     }
 }
 resource "null_resource" "fruits1" {
@@ -31,7 +29,7 @@ resource "null_resource" "fruits1" {
   provisioner "local-exec" {
     command = "echo Fruit Name - $(each.key) - (each.value)"
   }
-}
+}*/
 
 variable "fruits" {
   default = {
