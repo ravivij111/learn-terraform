687470f9f717cf4c11d661076fbbde62e955cebf
Your Name <you@example.com>
2023-05-11 13:56:54 +0800

    Terraform

--- 02-Data-Sources/main.tf
+++ 02-Data-Sources/main.tf
@@ -1,10 +1,22 @@
 
+#data "aws_security_group" "selected" {
+#  ## id = var.security_group_id
+#  name = "Ravi_Secuity_All"
+#}
+
 data "aws_security_group" "selected" {
   ## id = var.security_group_id
   name = "Ravi_Secuity_All"
 }
 
 
+
 output "security_group_id" {
-  value = data.aws_security_group.selected.id
+  value =  data.aws_security_group.selected.id
+}
+
+data "aws_security_group" "all_security_groups" {}
+
+output "all_security_groups" {
+  value =  data.aws_security_group.all_security_groups
 }
