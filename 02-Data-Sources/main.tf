61b0d2493c74ac4c5344b67461541f205a23bd7c
Your Name <you@example.com>
2023-05-11 14:14:44 +0800

    Terraform

--- 02-Data-Sources/main.tf
+++ 02-Data-Sources/main.tf
@@ -19,3 +19,12 @@ data "aws_security_groups" "all_security_group" {}
 output "all_security_groups" {
   value =  data.aws_security_groups.all_security_group
 }
+
+data "aws_security_group" "single" {
+  count = length(data.aws_security_groups.all_security_group.ids)
+  id = data.aws_security_groups.all_security_group.ids[count.index]
+}
+
+output "single" {
+  value = data.aws_security_group.single
+}
