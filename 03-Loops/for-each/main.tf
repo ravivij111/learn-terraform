c52629da61ca41ad37721417907fbfbf12f85b1a
Your Name <you@example.com>
2023-05-12 05:46:14 +0800

    Terraform

--- 03-Loops/for-each/main.tf
+++ 03-Loops/for-each/main.tf
@@ -2,7 +2,23 @@
 #  count =10
 #
 #}
+# *************** Start of Count *************************
+#variable "fruits" {
+#  default = {
+#    "apple", "orange", "banana"
+#  }
+#}
+#
+#resource "null_resource" "fruits" {
+#  count = length(var.fruits)
+#  provisioner "local-exec" {
+#    command = "echo Fruit Name - ${var.fruits[count.index]}"
+#  }
+#}
+# *************** End of Count *************************
 
+
+# *************** Start of for_each *************************
 variable "fruits" {
   default = {
     apple = 10
@@ -10,10 +26,11 @@ variable "fruits" {
     banana =20
     }
 }
-
 resource "null_resource" "fruits" {
-  count = length(var.fruits)
+  for_each = var.fruits
   provisioner "local-exec" {
-    command = "echo Fruit Name - ${var.fruits[count.index]}"
+    command = "echo Fruit Name - ${var.fruits[each.key]} - ${var.fruits[each.value]}"
   }
 }
+
+# *************** End of for_each *************************
