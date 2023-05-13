fce15704e69deab8c24e6cd6247d19f65c32d370
Your Name <you@example.com>
2023-05-13 08:51:20 +0800

    Terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -18,3 +18,15 @@ output "sample" {
 output "sample1" {
   value = element(var.sample1,0)
 }
+
+variable "sample2" {
+
+  default = {
+     xyz = 100
+     abc = 200
+  }
+}
+// If a1 key does not find then print the value as dummy bu lookup
+output "sample2" {
+  value = lookup(var.sample2, "a1", "dummy")
+}
