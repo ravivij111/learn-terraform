95b5a7141e920b5eebac3627c390e70882863da7
Your Name <you@example.com>
2023-05-14 15:52:30 +0800

    Terraform

--- 03-Loops/for-each/main.tf
+++ 03-Loops/for-each/main.tf
@@ -59,7 +59,7 @@ variable "fruits" {
 resource "null_resource" "fruits" {
   for_each = var.fruits
   provisioner "local-exec" {
-     command = "echo **** Key Name - ${each.key} ************* '\n'  Fruit Name -  ${each.value["name"]} '\n'  Fruit count - ${each.value["count"]} '\n'  Fruit Color - ${each.value["color"]}"
+     command = "echo **** Key Name - ${each.key} ************* \n  Fruit Name -  ${each.value["name"]} \n  Fruit count - ${each.value["count"]} '\n'  Fruit Color - ${each.value["color"]}"
   }
 }
 
