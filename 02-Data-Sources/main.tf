81e1d29d98e8719948cc25795c29d6b6ff0d166a
Your Name <you@example.com>
2023-05-22 14:38:57 +0800

    Terraform

--- 02-Data-Sources/main.tf
+++ 02-Data-Sources/main.tf
@@ -15,16 +15,17 @@
 #  value =  data.aws_security_group.selected.id
 #}
 
+// Get all Security Groups and print them all
 data "aws_security_groups" "all_security_group" {}
-/*output "all_security_groups" {
-  value =  data.aws_security_groups.all_security_group
-}*/
 
+output "all_security_groups" {
+  value =  data.aws_security_groups.all_security_group
+}
+// Retrieve and print the Security Groups one by one
 data "aws_security_group" "single" {
   count = length(data.aws_security_groups.all_security_group.ids)
   id = data.aws_security_groups.all_security_group.ids[count.index]
 }
-
 output "single" {
   value = data.aws_security_group.single
 }
