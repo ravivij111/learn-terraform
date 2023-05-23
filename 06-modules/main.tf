744a0bf1701de7def73e178794181e1f617490d6
Your Name <you@example.com>
2023-05-23 12:47:25 +0800

    Terraform

--- 06-modules/main.tf
+++ 06-modules/main.tf
@@ -1,12 +1,12 @@
 module "sample" {
   source = "./module"
-  input = "xyz"
+  input = module.dummy.dummy_op
   # the above input is a variable which should be declared in another file. We are passing this as input to the module
 }
 
 module "dummy" {
   source = "./dummy"
-  input = "xyz"
+
 }
 // Refer one module in anotehr module
 module "sample1" {
